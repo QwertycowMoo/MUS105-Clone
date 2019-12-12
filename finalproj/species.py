b26c8b842a74898990e8d76e24de896e35c3c982
Kevin Zhou <kjzhou2@illinois.edu>
2019-12-11 19:30:56 -0600

    Lots of Rules ConsecutiveInterval DirectInterval VoiceCrossing VoiceOverlap

--- finalproj/species.py
+++ finalproj/species.py
@@ -2,10 +2,11 @@
 
 ## You can import from score, theory, and any python system modules you want.
 
-from .score import Note, Pitch, Rest, Interval, Ratio, Mode, import_score
-from .theory import Analysis, Rule, timepoints
+from finalproj.score import Note, Pitch, Rest, Interval, Ratio, Mode, import_score
+from finalproj.theory import Analysis, Rule, timepoints
 from copy import copy
 from math import inf
+from glob import glob
 
 ## Settings for a species 1 analysis. Pass this to SpeciesAnalysis() if you
 # are analyzing a species 1 score. See also: SpeciesAnalysis.
@@ -46,7 +47,7 @@ s2_settings = copy(s1_settings)
 s2_settings['START_ABOVE'] = [1, 3, 5]
 s2_settings['MAX_4TH'] = inf  # no limit on melodic fourths
 s2_settings['MAX_5TH'] = inf  # no limit on melodic fifths
-s2_settings['MAX_UNI'] = 0    # no melodic unisons allowed
+s2_settings['MAX_UNI'] = 0  # no melodic unisons allowed
 
 ## A list of all the possible result strings your analysis can generate.
 # The {} marker in each string will always receive the 1-based integer index
@@ -57,40 +58,41 @@ s2_settings['MAX_UNI'] = 0    # no melodic unisons allowed
 # it simply contains the list of all the result strings ;)
 result_strings = [
     # VERTICAL RESULTS
-    'At #{}: consecutive unisons',#"parallel"
-    'At #{}: consecutive fifths',
-    'At #{}: consecutive octaves',
-    'At #{}: direct unisons',
-    'At #{}: direct fifths', # not parallel, move to a fifth when soprano leaps
-    'At #{}: direct octaves',
-    'At #{}: consecutive unisons in cantus firmus notes',  # if species 2 beginning of each measure
-    'At #{}: consecutive fifths in cantus firmus notes',   # if species 2
-    'At #{}: consecutive octaves in cantus firmus notes',  # if species 2
-    'At #{}: voice overlap',
-    'At #{}: voice crossing',
-    'At #{}: forbidden weak beat dissonance',   # vertical dissonance
-    'At #{}: forbidden strong beat dissonance', # vertical dissonance
-    'At #{}: too many consecutive parallel intervals',  # parallel vertical intervals
+    # make sure when you're pulling from this array that its count -1
+    'At #{}: consecutive unisons',  # 1 "parallel" done
+    'At #{}: consecutive fifths',  # 2 done
+    'At #{}: consecutive octaves',  # 3 done
+    'At #{}: direct unisons',  # 4 done
+    'At #{}: direct fifths',  # 5 not parallel, move to a fifth when soprano leaps done
+    'At #{}: direct octaves',  # 6 done
+    'At #{}: consecutive unisons in cantus firmus notes',  # 7 if species 2 beginning of each measure
+    'At #{}: consecutive fifths in cantus firmus notes',  # 8 if species 2
+    'At #{}: consecutive octaves in cantus firmus notes',  # 9 if species 2
+    'At #{}: voice overlap',  # 10
+    'At #{}: voice crossing',  # 11
+    'At #{}: forbidden weak beat dissonance',  # 12 vertical dissonance
+    'At #{}: forbidden strong beat dissonance',  # 13 vertical dissonance
+    'At #{}: too many consecutive parallel intervals',  # 14 parallel vertical intervals
 
     # MELODIC RESULTS
-    'At #{}: forbidden starting pitch',
-    'At #{}: forbidden rest', 
-    'At #{}: forbidden duration',   
-    'At #{}: missing melodic cadence',
-    'At #{}: forbidden non-diatonic pitch',
-    'At #{}: dissonant melodic interval',
-    'At #{}: too many melodic unisons',         # 'MAX_UNI' setting
-    'At #{}: too many leaps of a fourth',       # 'MAX_4TH' setting
-    'At #{}: too many leaps of a fifth',        # 'MAX_5TH' setting
-    'At #{}: too many leaps of a sixth',        # 'MAX_6TH' setting
-    'At #{}: too many leaps of a seventh',      # 'MAX_7TH' setting
-    'At #{}: too many leaps of an octave',      # 'MAX_8VA' setting
-    'At #{}: too many large leaps',             # 'MAX_LRG' setting
-    'At #{}: too many consecutive leaps'        # 'MAX_CONSEC_LEAP' setting
-    'At #{}: too many consecutive intervals in same direction', # 'MAX_SAMEDIR' setting
-    'At #{}: missing reverse by step recovery', # 'STEP_THRESHOLD' setting
-    'At #{}: forbidden compound melodic interval',
-    ]
+    'At #{}: forbidden starting pitch',  # 15
+    'At #{}: forbidden rest',  # 16
+    'At #{}: forbidden duration',  # 17
+    'At #{}: missing melodic cadence',  # 18
+    'At #{}: forbidden non-diatonic pitch',  # 19
+    'At #{}: dissonant melodic interval',  # 20
+    'At #{}: too many melodic unisons',  # 21 'MAX_UNI' setting
+    'At #{}: too many leaps of a fourth',  # 22 'MAX_4TH' setting
+    'At #{}: too many leaps of a fifth',  # 23 'MAX_5TH' setting
+    'At #{}: too many leaps of a sixth',  # 24 'MAX_6TH' setting
+    'At #{}: too many leaps of a seventh',  # 25 'MAX_7TH' setting
+    'At #{}: too many leaps of an octave',  # 26 'MAX_8VA' setting
+    'At #{}: too many large leaps',  # 27 'MAX_LRG' setting
+    'At #{}: too many consecutive leaps'  # 28 'MAX_CONSEC_LEAP' setting
+    'At #{}: too many consecutive intervals in same direction',  # 29 'MAX_SAMEDIR' setting
+    'At #{}: missing reverse by step recovery',  # 30 'STEP_THRESHOLD' setting
+    'At #{}: forbidden compound melodic interval',  # 31
+]
 
 
 ## A class that implements a species counterpoint analysis of a given score.
@@ -121,14 +123,37 @@ class SpeciesAnalysis(Analysis):
         ## A local copy of the analysis settings.
         self.settings = copy(s1_settings) if species == 1 else copy(s2_settings)
         ## Add your rules to this list.
-        self.rules = []
+        self.rules = [MelodicCadence(self), StartNote(self), ConsecutiveInterval(self, 1), ConsecutiveInterval(self, 5), ConsecutiveInterval(self, 8),
+                      DirectInterval(self, 1), DirectInterval(self, 5), DirectInterval(self, 8), VoiceCrossing(self), VoiceOverlap(self)]
         ## A list of strings that represent the findings of your analysis.
         self.results = []
 
+        self.cpMelody = []
+        self.cfMelody = []
+
     ## Use this function to perform whatever setup actions your rules require.
     def setup(self, args, kwargs):
-        pass
-    
+        tps = timepoints(self.score, span=True, measures=False)
+        topMelody = []
+        bottomMelody = []
+        for t in tps:
+            topMelody.append(t.nmap['P1.1'])
+        for t in tps:
+            bottomMelody.append(t.nmap['P2.1'])
+
+        if self.score.get_part('P1').name == 'CP':
+            self.cpMelody = topMelody
+            self.cfMelody = bottomMelody
+            self.cpIsTop = True
+        elif self.score.get_part('P2').name == 'CP':
+            self.cpMelody = bottomMelody
+            self.cfMelody = topMelody
+            self.cpIsTop = False
+        cpZipMelody = list(zip(self.cpMelody[:-1], self.cpMelody[1:]))
+        self.cpIntervals = [Interval(cpZipMelody[i][0].pitch, cpZipMelody[i][1].pitch) for i in range(len(cpZipMelody))]
+        self.verticalIntervals = [Interval(bottomMelody[i].pitch, topMelody[i].pitch) for i in
+                                  range(len(topMelody))]
+
     ## This function is given to you, it returns your analysis results
     # for the autograder to check.  You can also use this function as
     # a top level call for testing. Just make sure that it always returns
@@ -138,7 +163,167 @@ class SpeciesAnalysis(Analysis):
         ## When you return your results to the autograder make sure you convert
         # it to a Python set, like this:
         return set(self.results)
-    
+
+
+class StartNote(Rule):
+    def __init__(self, analysis):
+        super().__init__(analysis, "Checks if the starting note is correct or just a rest")
+        self.analysis = analysis
+        self.score = analysis.score
+        self.scale = self.score.metadata['main_key'].scale()
+        self.setting = analysis.settings
+
+    def apply(self):
+        cpMelody = self.analysis.cpMelody
+        cpIsTop = self.analysis.cpIsTop
+        self.results = self.analysis.results
+        if cpIsTop:
+            startingPC = self.setting['START_ABOVE']
+        else:
+            startingPC = self.setting['START_BELOW']
+        startingPitch = cpMelody[0].pitch.pnum()
+        startingNotes = [self.scale[i - 1] for i in startingPC]
+        if startingPitch not in startingNotes:
+            self.results.append(addToResults(0, result_strings[14]))
+
+class MelodicCadence(Rule):
+    def __init__(self, analysis):
+        super().__init__(analysis, "Checks for the melodic cadence in both voices")
+        self.analysis = analysis
+        self.score = analysis.score
+        self.key = self.score.metadata['main_key']
+        self.scale = self.key.scale()
+        self.setting = analysis.settings
+
+    def apply(self):
+        self.results = self.analysis.results
+        cadPatterns = self.setting['CADENCE_PATTERNS']  # [[2,1],[7,1]]
+        cadencePatterns = []
+        for i in range(len(cadPatterns)):
+            pitchPattern = []
+            for j in range(len(cadPatterns[i])):
+                #if the key is minor we have to raise the 7th
+                if self.key.mode == Mode.AEOLIAN and cadPatterns[i][j] == 7:
+                    pitchPattern.append(Interval('+1').transpose(self.scale[cadPatterns[i][j] - 1]))
+                else:
+                    pitchPattern.append(self.scale[cadPatterns[i][j] - 1])
+            cadencePatterns.append(pitchPattern)
+        cpLast2 = [self.analysis.cpMelody[i].pitch.pnum() for i in range(-2, 0)]
+        cfLast2 = [self.analysis.cfMelody[i].pitch.pnum() for i in range(-2, 0)]
+        if cpLast2 in cadencePatterns:
+            del cadencePatterns[cadencePatterns.index(cpLast2)]
+            if cfLast2 not in cadencePatterns:
+                self.results.append(addToResults(len(self.analysis.cpMelody) - 2, result_strings[17]))
+        else:
+            self.results.append(addToResults(len(self.analysis.cpMelody) - 2, result_strings[17]))
+
+class ConsecutiveInterval(Rule):
+    def __init__(self, analysis, interval):
+        super().__init__(analysis, "Checks for any consecutive (parallel) perfect intervals")
+        self.analysis = analysis
+        self.score = analysis.score
+        self.setting = analysis.settings
+        if interval not in [1, 5, 8]:
+            raise ValueError("The illegal consecutive interval must be a unison, fifth, or octave")
+        self.illegalInterval = interval
+
+    def apply(self):
+        self.results = self.analysis.results
+        verticalIntervals = self.analysis.verticalIntervals
+        zipInt = list(zip(verticalIntervals[:-1], verticalIntervals[1:]))
+        for i in range(len(zipInt)):
+            pair = zipInt[i]
+            if (self.illegalInterval == 1):
+                if (pair[0].is_unison()):
+                    if (pair[1].is_unison()):
+                        self.results.append(addToResults(i + 1, result_strings[0]))
+            elif (self.illegalInterval == 5):
+                if (pair[0].is_fifth()):
+                    if (pair[1].is_fifth()):
+                        self.results.append(addToResults(i + 1, result_strings[1]))
+            elif (self.illegalInterval == 8):
+                if (pair[0].is_octave()):
+                    if (pair[1].is_octave()):
+                        self.results.append(addToResults(i + 1, result_strings[2]))
+
+class DirectInterval(Rule):
+    def __init__(self, analysis, interval):
+        super().__init__(analysis, "Checks for any direct perfect intervals")
+        self.analysis = analysis
+        self.score = analysis.score
+        if interval not in [1, 5, 8]:
+            raise ValueError("The illegal direct interval must be a unison, fifth, or octave")
+        self.illegalInterval = interval
+
+    def apply(self):
+        self.results = self.analysis.results
+        verticalIntervals = self.analysis.verticalIntervals
+        #checks if the counterpoint is top if not, uses the cf melody as the soprano voice
+        if self.analysis.cpIsTop:
+            topMelody = self.analysis.cpMelody
+            topZipMelody = list(zip(topMelody[:-1], topMelody[1:]))
+            melodicIntervals = [Interval(topZipMelody[i][0].pitch, topZipMelody[i][1].pitch) for i in range(len(topZipMelody))]
+        else:
+            topMelody = self.analysis.cfMelody
+            topZipMelody = list(zip(topMelody[:-1], topMelody[1:]))
+            melodicIntervals = [Interval(topZipMelody[i][0].pitch, topZipMelody[i][1].pitch) for i in
+                                range(len(topZipMelody))]
+        zipVertical = list(zip(verticalIntervals[:-1], verticalIntervals[1:]))
+
+        #goes through both the pairs of vertical intervals if not parallel fifth but still fifth,
+        #then checks melodic interval for a leap
+        for i in range(len(zipVertical)):
+            firstI = zipVertical[i][0]
+            secondI = zipVertical[i][1]
+            if self.illegalInterval == 1:
+                if secondI.is_unison() and not firstI.is_unison():
+                    if melodicIntervals[i] > Interval('M2'):
+                        self.results.append(addToResults(i + 1, result_strings[3]))
+            if self.illegalInterval == 5:
+                if secondI.is_fifth() and not firstI.is_fifth():
+                    if melodicIntervals[i] > Interval('M2'):
+                        self.results.append(addToResults(i + 1, result_strings[4]))
+            if self.illegalInterval == 8:
+                if secondI.is_octave() and not firstI.is_octave():
+                    if melodicIntervals[i] > Interval('M2'):
+                        self.results.append(addToResults(i + 1, result_strings[5]))
+
+class VoiceCrossing(Rule):
+    def __init__(self, analysis):
+        super().__init__(analysis, "Checks for voice overlap between the two voices")
+        self.analysis = analysis
+        self.score = analysis.score
+        self.setting = analysis.settings
+
+    def apply(self):
+        self.results = self.analysis.results
+        verticalIntervals = self.analysis.verticalIntervals
+        for i in range(len(verticalIntervals)):
+            if verticalIntervals[i].is_descending():
+                self.results.append(addToResults(i, result_strings[10]))
+
+class VoiceOverlap(Rule):
+    def __init__(self, analysis):
+        super().__init__(analysis, "Checks for voice overlap between the two voices")
+        self.analysis = analysis
+        self.score = analysis.score
+        self.setting = analysis.settings
+
+    def apply(self):
+        self.results = self.analysis.results
+        cpMelody = self.analysis.cpMelody
+        cfMelody = self.analysis.cfMelody
+        verticalIntervals = self.analysis.verticalIntervals
+        for i in range(len(cpMelody) - 1):
+            print(cpMelody[i + 1], cfMelody[i])
+            if cpMelody[i + 1] < cfMelody[i] and self.analysis.cpIsTop:
+                self.results.append(addToResults(i + 1, result_strings[9]))
+            if cpMelody[i + 1] > cfMelody[i] and not self.analysis.cpIsTop:
+                self.results.append(addToResults(i + 1, result_strings[9]))
+def addToResults(tp, resultString):
+    return resultString.format(tp + 1)
+
+
 ###############################################################################
 
 # A short list of files that contain lots of issues (see comments below)
@@ -204,3 +389,8 @@ samples = ['2-034-A_zawang2.musicxml', '2-028-C_hanzhiy2.musicxml', '2-000-B_sz1
 #     '2-029-A_hanzhiy2.musicxml'
 #     '2-010-B_mamn2.musicxml'
 
+scorePaths = glob("C:/Users/qwert/School/MUS105/kjzhou2/finalproj/Species/1-019-A_ajyanez2.musicxml")
+s = import_score(scorePaths[0])
+print(s)
+a = SpeciesAnalysis(s, 1)
+print(a.submit_to_grading())
